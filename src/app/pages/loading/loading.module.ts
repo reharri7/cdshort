3688d3feb41235e1e14cdc8444218aa164f31507
Billy <wbratz1@gmail.com>
2023-04-19 20:17:33 -0600

    Adds loading spinner

--- src/app/pages/loading/loading.module.ts
+++ src/app/pages/loading/loading.module.ts
@@ -1,20 +1,24 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {FormsModule} from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
-import {IonicModule} from '@ionic/angular';
+import { IonicModule } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
 
-import {LoadingPageRoutingModule} from './loading-routing.module';
-
-import {LoadingPage} from './loading.page';
+import { LoadingPage } from './loading.page';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    LoadingPageRoutingModule
+    RouterModule.forChild([
+      {
+        path: '',
+        component: LoadingPage,
+      },
+    ]),
   ],
-  declarations: [LoadingPage]
+  declarations: [LoadingPage],
 })
 export class LoadingPageModule {}
