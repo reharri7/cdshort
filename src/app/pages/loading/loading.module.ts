61c7a0bd4de6b5db5f88e6d4b142c372a66c80fa
Rhett Harrison <reharri7@asu.edu>
2023-04-21 12:48:29 -0700

    Billybratz/add loading component (#28)
    * Adds loading spinner
    
    * Fixed routing module
    
    * Updates loading page and scss file
    
    * Reverts changes to loading.module.ts
    
    ---------
    
    Co-authored-by: Billy <wbratz1@gmail.com>

--- src/app/pages/loading/loading.module.ts
+++ src/app/pages/loading/loading.module.ts
@@ -1,20 +1,15 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {FormsModule} from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
-import {IonicModule} from '@ionic/angular';
+import { IonicModule } from '@ionic/angular';
 
-import {LoadingPageRoutingModule} from './loading-routing.module';
+import { LoadingRoutingModule } from './loading-routing.module';
 
-import {LoadingPage} from './loading.page';
+import { LoadingPage } from './loading.page';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    LoadingPageRoutingModule
-  ],
-  declarations: [LoadingPage]
+  imports: [CommonModule, FormsModule, IonicModule, LoadingRoutingModule],
+  declarations: [LoadingPage],
 })
 export class LoadingPageModule {}
